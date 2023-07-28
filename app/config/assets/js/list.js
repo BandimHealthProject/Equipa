e99df5bf65c462e0d7d9db56e71488746c2a66aa
Ane Fisker <afisker@health.sdu.dk>
2023-07-28 21:32:15 +0200

    Working on index

--- app/config/assets/js/list.js
+++ app/config/assets/js/list.js
@@ -18,7 +18,7 @@ function display() {
     morNome = util.getQueryParameter('morNome');
 
     var head = $('#main');
-    head.prepend("<h1>" + mor + " - " + morNome +  </br> <h3> "Mulheres");
+    head.prepend("<h1>" + mor + " - " + morNome + " </br> <h3> Mulheres");
     
     doSanityCheck();
     loadMIF();
@@ -34,7 +34,7 @@ function loadMIF() {
     console.log("user:" + user);
     var sql = "SELECT MOR, NOMEMUL, ESTADO, EXITDATA, FOGAO, MIFDNASC, MOR, REG, REGDIA, REGID, RELA1" 
         " FROM MIF" + 
-        " WHERE REG = " + reg + " AND TAB = " + tab + " AND MOR = " + MOR + 
+        " WHERE REG = " + reg + " AND TAB = " + tab + " AND MOR = " + mor + 
         " ORDER BY FOGAO, REGID";
     //}
     
@@ -44,18 +44,24 @@ function loadMIF() {
     var successFn = function( result ) {
         console.log("Found " + result.getCount() + " MIF");
         for (var row = 0; row < result.getCount(); row++) {
+           // Her hentes alle de variabler fra SQL ind i javascript
+            var rowId = result.getData(row,"_id");                      // row række id (=_id i ODK) 
+            var savepoint = result.getData(row,"_savepoint_type")       // Savepoint = Finalize vs incomplete osv
             var MOR = result.getData(row,"MOR");
             var NOMEMUL = result.getData(row,"NOMEMUL");
             var REG = result.getData(row,"REG");
             var REGDIA = result.getData(row,"REGDIA");
             var TAB = result.getData(row,"TAB");
-    
-            var p = {type: 'woman', MOR, NOMEMUL, REG, REGDIA, TAB};
-            MIF.push(p);
+            var REGID = result.getData(row,"REGID")
+            // har samles alle variablerne i et "object"
+            var p = {type: 'woman',  rowId, savepoint, MOR, NOMEMUL, REG, REGDIA, REGID, TAB};
+            MIF.push(p); // Her tilføjes "object" til listen "pregnancies", der kommer til at indeholde alle graviditeterne der kommer frem fra SQL-koden
         }
         console.log("Women:", MIF)
-        populateView();
-        return;
+        
+         // så kaldes næste funktion, der henter børnene
+         loadChildren();
+         return;
     }
     var failureFn = function( errorMsg ) {
         console.error('Failed to get pregnancies from database: ' + errorMsg);
@@ -66,14 +72,82 @@ function loadMIF() {
     odkData.arbitraryQuery('MIF', sql, null, null, null, successFn, failureFn);
 }
 
-// add children here when get there
+// funktion der henter alle children fra SQL og gemmer dem i children-variablen
+function loadChildren() {
+    // SQL to get chidlren
+    var sql = "SELECT _id, _savepoint_type, DATASEG, DOB, HCAREA, IDCRI, IDMUL, MOR, NOMECRI, REG, REGDIA, SEX, TAB" +
+    " FROM CHILDREN" + 
+    " WHERE REG = " + reg + " AND HCAREA = " + hcarea + " AND TAB = " + tab +
+    " ORDER BY MOR, NOMECRI";
+    
+    children = [];
+    console.log("Querying database for children..."); // printer til log
+    console.log(sql); // printer til log
+    var successFn = function( result ) {
+        console.log("Found " + result.getCount() + " children");        // printer til log
+        for (var row = 0; row < result.getCount(); row++) {             // loop over alle rækker
+            // Her hentes alle de variabler fra SQL ind i javascript
+            var rowId = result.getData(row,"_id");                      // row ID 
+            var savepoint = result.getData(row,"_savepoint_type")
 
+            var DATASEG = result.getData(row,"DATASEG");
+            var DOB = result.getData(row,"DOB");
+            var HCAREA = result.getData(row,"HCAREA");
+            var REGIDC = result.getData(row,"REGIDC");
+            var REGID = result.getData(row,"REGID");                    // Kan ændres til var REGID = result.getData(row,"regid"), hvis det nu er ID'et
+            var MOR = result.getData(row,"MOR");
+            var NOMECRI = result.getData(row,"NOMECRI");
+            var REG = result.getData(row,"REG");
+            var REGDIA = result.getData(row,"REGDIA");
+            var SEX = result.getData(row,"SEX");
+            var TAB = result.getData(row,"TAB");
+
+            // har samles alle variablerne i et "object"
+            var p = {type: 'child', rowId, savepoint, DATASEG, DOB, HCAREA, MOR, NOMECRI, REG, REGDIA, REGID, REGIDC, SEX, TAB};
+            children.push(p);// Her tilføjes "object" til listen "children", der kommer til at indeholde alle børnene der kommer frem fra SQL-koden
+        }
+        console.log("Children:", children) // printer til log
+
+        // så kaldes næste funktion, der samler børn og kvinder i én liste
+        combinedList();
+        return;
+    }
+    // denne funktion smider en masse fejlmeddelser i log, hvis der er noget galt med SQL-koden
+    var failureFn = function( errorMsg ) {
+        console.error('Failed to get children from database: ' + errorMsg);
+        console.error('Trying to execute the following SQL:');
+        console.error(sql);
+        alert("Program error Unable to look up children.");
+    }
+    // denne funktion tjekker om der er noget galt med SQL-koden
+    odkData.arbitraryQuery('CHILDREN', sql, null, null, null, successFn, failureFn);
+}
+
+// funktion der samaler børn og kvinder i samme liste, og ordner dem, så børn står under deres mor
+// her bruges varablen "IDMUL" fra linje 64 og 113. Derfor vigtigt, hvad man har kaldt variablen
 function combinedList() {
     // make combined list
     personList = [];
-    MIF.forEach(function(preg) {
+    MIF.forEach(function(preg) { // VIGTIGT: "pregnacies" skal være listen med kvinderne
         personList.push(preg);
-    }); 
+        var thisPregChild = children.filter(function(obj) { // VIGTIGT: "children" skal være listen med børnene
+            return obj.REGID == preg.REGID;
+        });
+        thisPregChild.forEach(function(child) {
+            personList.push(child);
+            children.splice(children.findIndex(function(obj) {
+                return obj.REGID == preg.REGID;
+            }), 1);
+        });
+    });
+
+    // Add any remaining (orphaned) children to the beginning of the list - hence 'unshift'
+    children.forEach(function(child) {  
+        personList.unshift(child);     
+    });
+    console.log('personList', personList); // printer til log
+
+    // så kaldes næste funktion, der danner HTML-kode, så der kommer en liste med kvinder og børn
     populateView();
 }
 
@@ -89,42 +163,88 @@ function populateView() {
         openFormNewPreg();
     })
     
+   
+      // Så lave listen over alle kvinder og børn
     var ul = $('#li');
-
-    // list
     $.each(personList, function() {
         var that = this; 
         
         // Check if visited today
         var option = '';
-        if ((this.CONT == todayAdate | this.REGDIA == todayAdate) & this.savepoint == "COMPLETE") {
+        if ((this.DATASEG == todayAdate | this.REGDIA == todayAdate) & this.savepoint == "COMPLETE") {
             option = "visited";
         }
-      
+
         // set text to display
+        // funktion der danner teksten som man ser på knapperne
         var displayText = setDisplayText(that);
         
         // list
-        // preg criterias
-          
+        // Hvis kvinde (pregnacy)
+        if (this.type == "MIF") {
+            ul.append($("<li />").append($("<button />").attr('id',this.REGID).attr('class', option + " " + this.type).append(displayText)));
+         
+            // Buttons
+            var btn = ul.find('#' + this.REGID);
+            btn.on("click", function() {
+                // hvis man trykker på knappen kaldes funktionen der åbner formen
+                openForm(that);
+            }) 
+        } 
+        // Hvis barn
+        if (this.type == "child") {
+            ul.append($("<li />").append($("<button />").attr('id',this.REGIDC).attr('class', option + " " + this.type + " sex" + this.SEX).append(displayText)));
+        
+            // Buttons
+            var btn = ul.find('#' + this.REGIDC);
+            btn.on("click", function() {
+                // hvis man trykker på knappen kaldes funktionen der åbner formen
+                openForm(that);
+            }) 
+        }       
+    });
+}
 
-
+// funktionen der laver tekst til knapperne (elementerne i listen)
 function setDisplayText(person) {
     var displayText, regdia;
     
-    if (person.type == "MIF") {
+    // hvis kvinde (pregnancy)
+    if (person.type == "pregnancy") {
+        // funtion der formaterer dato til noget der er pænt at se på
         regdia = formatDate(person.REGDIA);
-        var obs = "";
+        
+        // teksten
+        displayText = "Morança: " + person.MOR + "<br />" +
+        "Nome: " + person.NOMEMUL + "<br />" + 
+        "Dia de inclusão: " + regdia + "<br />" +
+        "ID mulher: " + person.REGID;
+        
+        
+    } else { // hvis barn
+        // funtion der formaterer dato til noget der er pænt at se på
+        regdia = formatDate(person.REGDIA);
+        var dob = formatDate(person.DOB);
+        
+        var sex = "Não sabe";
+        if (person.SEX == 1) {
+            sex = "Masculino";
+        } else if (person.SEX == 2) {
+            sex = "Fêmea";
+        } 
 
-{
-            displayText = "Morança: " + person.MOR + "<br />" +
-            "Nome: " + person.NOMEMUL + "<br />" + 
+        // teksten
+        displayText = "Morança: " + person.MOR + "<br />" +
+            "Nome da criança: " + person.NOMECRI + "<br />" + 
+            "Sexo: " + sex + "<br />" + 
+            "Dia de nascimento: " + dob + "<br />" + 
             "Dia de inclusão: " + regdia + "<br />" +
-                 }
-
+            "ID criança: " + person.REGIDC; 
+    }
     return displayText
 }
 
+// funtktionen der laver datoer pæne
 function formatDate(adate) {
     if (adate == null) {
         var date = " "
@@ -139,9 +259,11 @@ function formatDate(adate) {
     
 }
 
-function openFormMIF() {
-    console.log("Preparing form for new pregnancy");
+// funktionen der kaldes hvis man trykker på knap for ny graviditet
+function openFormNewPreg() {
+    console.log("Preparing form for new pregnancy"); // printer til log
 
+    // her sættes de værdier, der automatisk skal skrives, når man starter en ny form(/række i SQL)
     var defaults = {};
     defaults['HCAREA'] = hcarea;
     defaults['HCAREANOME'] = hcareaNome;
@@ -151,12 +273,125 @@ function openFormMIF() {
     defaults['TAB'] = tab;
     defaults['TABNOME'] = tabNome;
     
-    console.log("Opening form with: ", defaults); 
+    console.log("Opening form with: ", defaults); // printer til log
+    
+    // ODK-funktion der tilføje ny række til SQL-tabel 
     odkTables.addRowWithSurvey(
         null,
-        'MIF_VISIT',
-        'MIF_VISIT',
+        'MIF', // navn på table i SQL
+        'MIF_VISIT', // navn på den form der skal åbnes
         null,
         defaults);
 }
 
+
+// funktionen der kaldes hvis man trykker på en kvinde eller barn
+function openForm(person) {
+    console.log("Preparing form for: ", person); // printer til log
+    
+    // dags dato 
+    var today = new Date(date);
+    var todayAdate = "D:" + today.getDate() + ",M:" + (Number(today.getMonth()) + 1) + ",Y:" + today.getFullYear();
+
+    // række-id fra SQL-tabel, hvis man skal rætte i en række (en der allerede er besøgt)
+    var rowId = person.rowId;
+    
+    // hvis kvinde (pregnancy)
+    if (person.type == 'MIF') { // pregnancy
+        // hvis besøgt allerede (registeret i dag), så skal man rette i en række, der allerede eksisterer
+        if (person.REGDIA == todayAdate) {
+            console.log("Edit form for pregnancy: ", person) // printer til log
+            odkTables.editRowWithSurvey(
+                null,
+                'MIF_VISIT',  // navn på table i SQL
+                rowId,          //_id for rækken man vil ændre
+                'MIF_VISIT',  // navn på den form der skal åbnes
+                null,);
+        // hvis besøgt allerede, så skal man rette i en række, der allerede eksisterer
+        } else if (person.DATASEG == todayAdate) {
+            console.log("Edit form for pregnancy: ", person) // printer til log
+            odkTables.editRowWithSurvey(
+                null,
+                'MIF_VISIT',  // navn på table i SQL
+                rowId,          //_id for rækken man vil ændre
+                'MIF_VISIT',  // navn på den form der skal åbnes
+                null,);
+        // hvis ikke besøgt, skal man lave en ny række i SQL (som med openFromNewPreg()-funktionen)
+        } else {
+            // her sættes de værdier, der automatisk skal skrives, når man starter en ny form(/række i SQL) - denne gang er der bare en seperat funktion til dette
+            var defaults = getDefaults(person);
+            console.log("Opening pregnancy follow-up form with: ", defaults); // printer til log
+            odkTables.addRowWithSurvey(
+                null,
+                'MIF_VISIT', // navn på table i SQL
+                'MIF_VISIT', // navn på den form der skal åbnes
+                null,
+                defaults);
+        }
+    // hvis barn
+    } else { // child
+        // hvis besøgt allerede, så skal man rette i en række, der allerede eksisterer
+        if (person.DATASEG == todayAdate) {
+            console.log("Edit form for child: ", person) // printer til log
+            odkTables.editRowWithSurvey(
+                null,
+                'CHILDREN', // navn på table i SQL
+                rowId,      //_id for rækken man vil ændre
+                'CHILDREN', // navn på den form der skal åbnes
+                null,);
+        // hvis ikke besøgt, skal man lave en ny række i SQL (som med openFromNewPreg()-funktionen)
+        } else {
+            // her sættes de værdier, der automatisk skal skrives, når man starter en ny form(/række i SQL) - denne gang er der bare en seperat funktion til dette
+            var defaults = getDefaults(person);
+            console.log("Opening child follow-up form with: ", defaults); // printer til log
+            odkTables.addRowWithSurvey(
+                null,
+                'CHILDREN', // navn på table i SQL
+                'CHILDFU', // navn på den form der skal åbnes
+                null,
+                defaults);
+        }
+    }
+}
+
+// laber dato til "adate"-format
+function toAdate(date) {
+    var jsDate = new Date(date);
+    return "D:" + jsDate.getDate() + ",M:" + (Number(jsDate.getMonth()) + 1) + ",Y:" + jsDate.getFullYear();
+}
+
+// funktionen der sætter de værdier, der automatisk skal skrives, når man starter en ny form(/række i SQL) 
+function getDefaults(person) {
+    var defaults = {};
+    // hvis kvinde
+    if (person.type == "MIF") { // pregnancy defaults 
+        defaults['DATASEG'] = toAdate(date);
+        defaults['HCAREA'] = hcarea;
+        defaults['HCAREANOME'] = hcareaNome;
+        defaults['REGID'] = person.REGID;
+        defaults['MOR'] = person.MOR;
+        defaults['NOMEMUL'] = person.NOMEMUL;
+        defaults['REGNOME'] = regNome;
+        defaults['REGDIA'] = person.REGDIA;
+        defaults['TAB'] = tab;
+        defaults['TABNOME'] = tabNome;
+    // hvis barn
+    } else { // child defaults
+        defaults['DATASEG'] = toAdate(date);
+        defaults['DOB'] = person.DOB;
+        defaults['HCAREA'] = hcarea;
+        defaults['HCAREANOME'] = hcareaNome;
+        defaults['REGIDC'] = person.REGIDC;
+        defaults['REGID'] = person.REGID;
+        defaults['MOR'] = person.MOR;
+        defaults['NOMECRI'] = person.NOMECRI;
+        defaults['REG'] = reg;
+        defaults['REGNOME'] = regNome;
+        defaults['REGDIA'] = person.REGDIA;
+        defaults['SEX'] = person.SEX;
+        defaults['TAB'] = tab;
+    }
+    return defaults;
+}
+   
+ 
